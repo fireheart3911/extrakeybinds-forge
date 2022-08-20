b60d272d5ef7747b09f42ca39d7e768e853856a7
fireheart3911 <64578352+fireheart3911@users.noreply.github.com>
2022-08-20 15:04:33 +0200

    Push Files

---
+++ main/java/net/fireheart3911/extrakeybindsforge/ExtraKeybindsForge.java
@@ -0,0 +1,28 @@
+package net.fireheart3911.extrakeybindsforge;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(ExtraKeybindsForge.MOD_ID)
+public class ExtraKeybindsForge
+{
+    public static final String MOD_ID = "extrakeybindsforge";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public ExtraKeybindsForge()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+    }
+
+}
---
+++ main/java/net/fireheart3911/extrakeybindsforge/event/ClientEvents.java
@@ -0,0 +1,36 @@
+package net.fireheart3911.extrakeybindsforge.event;
+
+import net.fireheart3911.extrakeybindsforge.ExtraKeybindsForge;
+import net.fireheart3911.extrakeybindsforge.util.KeyBinding;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+public class ClientEvents {
+    @Mod.EventBusSubscriber(modid = ExtraKeybindsForge.MOD_ID, value = Dist.CLIENT)
+    public static class ClientForgeEvents {
+
+        @SubscribeEvent
+        public static void onKeyInput(InputEvent.Key event){
+
+
+        }
+    }
+
+    @Mod.EventBusSubscriber(modid = ExtraKeybindsForge.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModBusEvents{
+        @SubscribeEvent
+        public static void onKeyRegister(RegisterKeyMappingsEvent event){
+            event.register(KeyBinding.TERTIARY_KEY);
+            event.register(KeyBinding.NONARY_KEY);
+            event.register(KeyBinding.OCTENARY_KEY);
+            event.register(KeyBinding.QUATERNARY_KEY);
+            event.register(KeyBinding.QUINARY_KEY);
+            event.register(KeyBinding.SENARY_KEY);
+            event.register(KeyBinding.SEPTENARY_KEY);
+        }
+    }
+
+}
